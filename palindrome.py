673248509d11972907b0b0bcd6a9a91bcadf6941
Ranimru <rannoosh6@gmail.com>
2015-11-19 22:44:04 +0200

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,5 @@
 def isPalindrome(String):
+   #this is a method to check whether a string is a Palindrome
    for s in range(0,len(String)//2):
       if String[s]!=String[(len(String)-1)-s]: 
           return False
