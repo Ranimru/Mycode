473e5a2e2f399a100cbfbc872a35b71d412de599
ranimru <ranimru@github.com>
2015-12-09 20:42:00 +0200

    splitting numbers

--- problem40.py
+++ problem40.py
@@ -1,35 +1,7 @@
-L=[]
-for i in range(0,190000):
-    x=str(i)
-    if x==0:
-     L.append(x)
-    if i<10:
-     L.append(x)
-    if i>=10 and i<100:
-     d=list(x)
-     for j in range(0,2):
-      L.append(d[j])
-    if i>=100 and i<1000:
-      d=list(x)
-      for j in range(0,3):
-       L.append(d[j])
-    if i>=1000 and i<10000:
-      d=list(x)
-      for j in range(0,4):
-       L.append(d[j])
-    if i>=10000 and i<100000:
-      d=list(x)
-      for j in range(0,5):
-       L.append(d[j])
-    if i>=100000 and i<1000000:
-      d=list(x)
-      for j in range(0,6):
-       L.append(d[j])
+L=[0,1]
+for i in range(2,2000000):
+    for j in str(i):
+        L.append(j)
 
-print L
 print int(L[1])*int(L[10])*int(L[100])*int(L[1000])*int(L[10000])*int(L[100000])*int(L[1000000])
-   
-
-
-  
-
+    
