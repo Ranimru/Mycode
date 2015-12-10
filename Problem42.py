75e840b8bcb204ae5426a8d495822399f0946519
ranimru <ranimru@github.com>
2015-12-10 15:45:14 +0200

    triangle numbers with list of words

---
+++ Problem42.py
@@ -0,0 +1,25 @@
+words=[]
+def Trianglenumbers(n):
+    TriangleNumbers=[]
+    for i in range(1,n):
+     t=(i*(i+1)/2)
+     TriangleNumbers.append(t)
+    return L
+
+import string
+alpha='#'
+alpha=alpha+string.uppercase
+
+for word in List:
+ wordindex=[]
+ for character in word:
+  index=alpha.index(character)
+  wordindex.append(index)
+ 
+ for s in t(1000):
+  
+  if sum(wordindex)==s:
+   words.append(word)
+
+print words
+print len(words)
